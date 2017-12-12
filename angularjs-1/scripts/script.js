3d0f6ae5ff89063aaa33b4285381f7a50465a114
ryanluoxu <luoxu2011@gmail.com>
2017-12-13 00:24:12 +0800

    add

--- angularjs-1/scripts/script.js
+++ angularjs-1/scripts/script.js
@@ -1,4 +1,4 @@
-var myApp = angular.module("myModule",[]);	// 1. create module	
+//var myApp = angular.module("myModule",[]);	// 1. create module	
 
 /**
 var myController = function ($scope) {		// 2. create controller
@@ -9,6 +9,38 @@ myApp.controller("myController", myController);		// 3. register controller with 
 
 /** combine 2 and 3 */
 
-myApp.controller("myController", function ($scope) {
-	$scope.message = "Hello world..";
-}); 
+//myApp.controller("myController", function ($scope) {
+//	
+//	var customer = {		
+//		firstName: "ryan",
+//		lastName: "luo",
+//		gender: "male"
+//	};							// create object
+//	
+//	$scope.customer = customer;	// attach object to scope
+//
+//}); 
+
+var myApp = angular
+				.module("myModule",[])
+				.controller("myController", function ($scope) {
+					
+					var customer = {
+							name: "ryan",
+							age: "29"
+					};
+					
+					$scope.customer = customer;
+				}); 
+
+
+
+
+
+
+
+
+
+
+
+
